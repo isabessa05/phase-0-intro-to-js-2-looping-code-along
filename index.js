d5c6dca88f1596c18faa7a191b2171ac7ce12144
+ <+>
2022-07-23 16:05:33 -0400

    commit tree d283a71dc2ce9a64dc2fbdf589c7d131d0361467

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(array, name){
+
+  let message = []
+  for (let i=0; i < array.length; i++) {
+    message.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+  }
+  console.log(message)
+  return message;
+}
+
+// writeCards(["Charlie", "Samip", "Ali"], "birthday")
+
+function countDown(num){
+  while (num >= 0 ){
+    console.log(num);
+    num--;
+}
+}
